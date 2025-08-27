f6dc42c826ff08600b31153ee54032f462a0e123
hc <0903006293asd@gmail.com>
2025-08-28 01:28:16 +0900

    j

---
+++ Animated_count/script.js
@@ -0,0 +1,21 @@
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+
+function resetDOM() {
+    counter.classList.remove('hide');
+    finalMessage.classList.remove('show');
+
+    nums.forEach((num) => {
+        num.classList.value = ''
+    });
+    nums[0].classList.add('in');
+}
+
+function runAnimation() {
+    nums.forEach((num, idx) => {
+
+    })
+}
